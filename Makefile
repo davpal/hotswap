eec8f60356970505c9b149286a9d78d26f99e42e
davpal <paluchdaw@gmail.com>
2018-11-05 16:47:16 -0500

    Load plugin library in main loop

--- Makefile
+++ Makefile
@@ -8,6 +8,7 @@ INITFUN=on_load
 plugin.so: plugin.cpp
 	$(CXX) $(CFLAGS) $(CXXFLAGS) -shared $< -o $@ -Wl,-init,$(INITFUN)
 
+LDFLAGS=-ldl
 hotswap: hotswap.cpp
 
 .PHONY: clean
--- hotswap.cpp
+++ hotswap.cpp
@@ -2,11 +2,22 @@
 #include <dlfcn.h>
 #include "plugin.h"
 
+void load_plugin();
+
 int main(int argc, char * argv[])
 {
     int c = 0;
     do {
         printf("Waiting for you sir: ");
         c = getchar();
+        load_plugin();
     } while(c != 'q');
+}
+
+void load_plugin()
+{
+    auto dl = dlopen("./plugin.so", RTLD_NOW);
+    if (!dl) {
+        fprintf(stderr, "Failed to load plugin %s.\n", dlerror());
+    }
 }
