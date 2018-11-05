0f4079438ae558ab0d04dd5630b108633f74140b
davpal <paluchdaw@gmail.com>
2018-11-05 16:37:00 -0500

    Add main user input loop

--- hotswap.cpp
+++ hotswap.cpp
@@ -1,6 +1,12 @@
+#include <cstdio>
 #include <dlfcn.h>
 #include "plugin.h"
 
 int main(int argc, char * argv[])
 {
+    int c = 0;
+    do {
+        printf("Waiting for you sir: ");
+        c = getchar();
+    } while(c != 'q');
 }
